ff044e303905c86820c717004cbf95a516aab5e3
Norman Basham <nbasham@yahoo.com>
2021-02-02 15:56:00 -0800

    Backed out platforms addition to Package.swift as it causes issues in Xcode. The platform info was added because it fixes the build errors reported on SwiftPackageIndex.

--- Package.swift
+++ Package.swift
@@ -5,9 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "BlackLabsSwiftUIColor",
-    platforms: [
-        .iOS(SupportedPlatform.IOSVersion.v11)
-    ],
+
+//  Fixes build errors on SwiftPackageIndex
+//    platforms: [
+//        .iOS(.v13)
+//    ],
+
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
