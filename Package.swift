cbaf5e1e8f6956a30353231c8a4340bad88c4091
Norman Basham <nbasham@yahoo.com>
2021-02-03 18:43:37 -0800

    Re-introducing platforms.

--- Package.swift
+++ Package.swift
@@ -7,9 +7,9 @@ let package = Package(
     name: "BlackLabsSwiftUIColor",
 
 //  Fixes build errors on SwiftPackageIndex
-//    platforms: [
-//        .iOS(.v13)
-//    ],
+    platforms: [
+        .iOS(.v13)
+    ],
 
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
